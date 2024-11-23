828e8540a1411af379bf58f0b72a323799d420cd
Millena <araujocavalcantimillena@gmail.com>
2024-11-23 17:01:46 -0300

    Add files via upload

---
+++ aula_5.py
@@ -0,0 +1,45 @@
+numero = 10
+print(numero)
+
+print('Olá boa tarde!')
+
+# 1 - instalar o python
+# 2 - instalar o vscode
+# 3 - instalar a extensão 
+
+#por padrão o input é um texto
+
+
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite um número: "))
+
+soma = numero1 + numero2
+#tipo = type(numero1)
+print(soma)
+
+# print(cumprimento, nome,'Sua senha é',senha)
+# print(f'{cumprimento} {nome} Sua senha é {senha}')
+# print(cumprimento + " "+ nome+ " " + 'Sua senha é',senha )
+# print(cumprimento, nome, 'Sua senha é {}'.format(senha))
+# print(cumprimento, nome, 'Sua senha é %s'%(senha))
+
+nome = input("Digite seu nome: ")
+print("SEJA BEM VINDO", nome)
+print("SEJA BEM VINDO(A)" + nome)
+
+print("SEJA BEM VINDO(A){}".format(nome))
+print("SEJA BEM VINDO(A) %s"%(nome) )
+
+texto = "Olá Mundo"
+print(len(texto))
+
+# int(), float(), str()
+
+n = True
+print(str(n))
+
+
+
+# Exercícios
+
+
